7ebe235728682425701fe3481f5ae5c1440cf6d7
Gabriel Moreira <gsm.br2003@gmail.com>
2023-05-29 21:13:08 -0300

    refactor: refacotr CRUD turma

--- tratadores.c
+++ tratadores.c
@@ -150,22 +150,18 @@ void tratador_menu_turma()
          }
          else
          {
-            salvar_turma_service();
+            cadastrarTurma();
          }
          break;
     case 2:
     {
-         // funcao para o case 2
-
-        buscar_turma_service();
+       exibirTurmas();
 
     }
     break;
     case 3:
     {
-         // funcao para o case 3
-
-        // atualizar_turma_service(construir_turma());
+        atualizarTurma();
     }
 
     break;
@@ -173,7 +169,7 @@ void tratador_menu_turma()
     {
          // funcao para o case 4
 
-        remover_turma_service();
+        excluirTurma();
     }
 
     break;
@@ -264,38 +260,6 @@ void remover_professor_service() {
     }
 
     return;
-}
-
-// turma services
-
-void salvar_turma_service() {
-
-    return cadastrarTurma();
-}
-
-void buscar_turma_service() {
-     char codigo[50];
-
-    printf("Digite o código da turma: ");
-    fgets(codigo, sizeof(codigo), stdin);
-    codigo[strcspn(codigo, "\n")] = '\0';
-
-    return imprimirTurma(codigo);
-}
-
-void atualizar_turma_service(Turma *turma)
-{
-    return atualizarTurmaRepository(turma);
-}
-
-void remover_turma_service() {
-
-    char codigo[50];
-    printf("Codigo > ");
-    fgets(codigo, 50, stdin);
-    putchar('\n');
-
-    return excluirTurmaRepository(codigo);
 }
 
 int printAllTeachesNoClass() {
@@ -434,16 +398,6 @@ void imprimir_professor(Professor *professor)
            professor->endereco->numero);
 }
 
-void imprimir_turma(Turma *turma)
-{
-    printf("Codigo: %s\n", turma->codigo);
-    printf("Disciplina: %s\n", turma->disciplina);
-    printf("Professor: %s\n", turma->professor_turma);
-    printf("Lista de alunos: %s\n", turma->lista_alunos);
-    printf("Media da turma: %s\n", turma->media_turma);
-}
-
-
 void imprimir_endereco(Endereco *endereco)
 {
     
@@ -469,6 +423,153 @@ void imprimirProfessoresNaoRelacionados(Turma* turmas, int numTurmas, Professor*
 }
 
 
+// Função para exibir as informações de uma turma
+void exibirTurma(const Turma* turma) {
+    printf("Código da Turma: %s\n", turma->codigo);
+    printf("Disciplina: %s\n", turma->disciplina);
+    printf("Professor: %s\n", turma->professor_turma);
+    printf("Lista de Alunos: %s\n", turma->lista_alunos);
+    printf("Média da Turma: %.2f\n", turma->media_turma);
+    printf("\n");
+}
+
+// Função para exibir todas as turmas cadastradas
+void exibirTurmas() {
+    // Abra o arquivo no modo de leitura binária
+    FILE* arquivo = fopen("db/turma.bin", "rb");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    Turma turma;
+
+    // Leia cada turma do arquivo e exiba suas informações
+    while (fread(&turma, sizeof(Turma), 1, arquivo) == 1) {
+        exibirTurma(&turma);
+    }
+
+    fclose(arquivo);
+}
+
+// Função para atualizar as informações de uma turma
+void atualizarTurma() {
+    char codigo[50];
+
+    printf("Digite o código da turma que deseja atualizar: ");
+    fgets(codigo, sizeof(codigo), stdin);
+    codigo[strcspn(codigo, "\n")] = '\0';
+
+    // Abra o arquivo no modo de leitura e escrita binária
+    FILE* arquivo = fopen("db/turma.bin", "r+b");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    Turma turma;
+
+    // Pesquise pela turma com o código informado
+    while (fread(&turma, sizeof(Turma), 1, arquivo) == 1) {
+        if (strcmp(turma.codigo, codigo) == 0) {
+            // Solicite ao usuário as novas informações da turma
+            printf("Digite o novo código da turma: ");
+            fgets(turma.codigo, sizeof(turma.codigo), stdin);
+            turma.codigo[strcspn(turma.codigo, "\n")] = '\0';
+
+            printf("Digite a nova disciplina: ");
+            fgets(turma.disciplina, sizeof(turma.disciplina), stdin);
+            turma.disciplina[strcspn(turma.disciplina, "\n")] = '\0';
+
+            printf("Digite o novo professor da turma: ");
+            fgets(turma.professor_turma, sizeof(turma.professor_turma), stdin);
+            turma.professor_turma[strcspn(turma.professor_turma, "\n")] = '\0';
+
+            printf("Digite a nova lista de alunos (separados por vírgula): ");
+            fgets(turma.lista_alunos, sizeof(turma.lista_alunos), stdin);
+            turma.lista_alunos[strcspn(turma.lista_alunos, "\n")] = '\0';
+
+            printf("Digite a nova média da turma: ");
+            scanf("%f", &turma.media_turma);
+            getchar(); // Limpar o buffer do enter
+
+            // Retorne para a posição do arquivo onde a turma está localizada
+            fseek(arquivo, -sizeof(Turma), SEEK_CUR);
+
+            // Escreva as novas informações da turma no arquivo
+            fwrite(&turma, sizeof(Turma), 1, arquivo);
+
+            printf("Turma atualizada com sucesso.\n");
+            break;
+        }
+    }
+
+    fclose(arquivo);
+}
+
+// Função para excluir uma turma
+void excluirTurma() {
+    char codigo[50];
+
+    printf("Digite o código da turma que deseja excluir: ");
+    fgets(codigo, sizeof(codigo), stdin);
+    codigo[strcspn(codigo, "\n")] = '\0';
+
+    // Abra o arquivo no modo de leitura e escrita binária
+    FILE* arquivo = fopen("db/turma.bin", "r+b");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    Turma turma;
+
+    FILE* arquivoTemp = fopen("temp.bin", "wb");
+    if (arquivoTemp == NULL) {
+        printf("Erro ao criar arquivo temporário.\n");
+        fclose(arquivo);
+        return;
+    }
+
+    // Copie todas as turmas que não têm o código informado para o arquivo temporário
+    while (fread(&turma, sizeof(Turma), 1, arquivo) == 1) {
+        if (strcmp(turma.codigo, codigo) != 0) {
+            fwrite(&turma, sizeof(Turma), 1, arquivoTemp);
+        }
+    }
+
+    fclose(arquivo);
+    fclose(arquivoTemp);
+
+    // Remova o arquivo original
+    remove("db/turma.bin");
+
+    // Renomeie o arquivo temporário para o nome original
+    rename("temp.bin", "db/turma.bin");
+
+    printf("Turma excluída com sucesso.\n");
+}
+
+void imprimirTurma(const char* codigo) {
+    // Abra o arquivo no modo de leitura binária
+    FILE* arquivo = fopen("db/turma.bin", "rb");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    Turma turma;
+
+    // Pesquise pela turma com o código informado
+    while (fread(&turma, sizeof(Turma), 1, arquivo) == 1) {
+        if (strcmp(turma.codigo, codigo) == 0) {
+            exibirTurma(&turma);
+            break;
+        }
+    }
+
+    fclose(arquivo);
+}
 
 void cadastrarTurma() {
     Turma turma;
@@ -476,6 +577,12 @@ void cadastrarTurma() {
     printf("Digite o código da turma: ");
     fgets(turma.codigo, sizeof(turma.codigo), stdin);
     turma.codigo[strcspn(turma.codigo, "\n")] = '\0';
+
+    // Verificar se o código da turma já existe
+    if (verificarCodigoTurma(turma.codigo)) {
+        printf("Já existe uma turma cadastrada com esse código.\n");
+        return;
+    }
 
     printf("Digite a disciplina: ");
     fgets(turma.disciplina, sizeof(turma.disciplina), stdin);
@@ -485,21 +592,22 @@ void cadastrarTurma() {
     fgets(turma.professor_turma, sizeof(turma.professor_turma), stdin);
     turma.professor_turma[strcspn(turma.professor_turma, "\n")] = '\0';
 
-    printf("Digite a média da turma: ");
-    scanf("%f", &turma.media_turma);
-    getchar(); // Limpar o buffer do enter
-
     printf("Digite a lista de alunos (separados por vírgula): ");
     fgets(turma.lista_alunos, sizeof(turma.lista_alunos), stdin);
     turma.lista_alunos[strcspn(turma.lista_alunos, "\n")] = '\0';
 
+    printf("Digite a média da turma: ");
+    scanf("%f", &turma.media_turma);
+    getchar(); // Limpar o buffer do enter
+
+    // Abra o arquivo no modo de escrita binária
     FILE* arquivo = fopen("db/turma.bin", "ab");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
 
-    // Escrever a estrutura Turma no arquivo binário
+    // Escreva a turma no arquivo
     fwrite(&turma, sizeof(Turma), 1, arquivo);
 
     fclose(arquivo);
@@ -507,26 +615,24 @@ void cadastrarTurma() {
     printf("Turma cadastrada com sucesso.\n");
 }
 
-void imprimirTurma(const char* codigo) {
+int verificarCodigoTurma(const char* codigo) {
+    // Abra o arquivo no modo de leitura binária
     FILE* arquivo = fopen("db/turma.bin", "rb");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.\n");
-        return;
+        return 0;
     }
 
     Turma turma;
 
+    // Verifique se há uma turma com o mesmo código
     while (fread(&turma, sizeof(Turma), 1, arquivo) == 1) {
         if (strcmp(turma.codigo, codigo) == 0) {
-            printf("Código da Turma: %s\n", turma.codigo);
-            printf("Disciplina: %s\n", turma.disciplina);
-            printf("Professor: %s\n", turma.professor_turma);
-            printf("Lista de Alunos: %s\n", turma.lista_alunos);
-            printf("Média da Turma: %.2f\n", turma.media_turma);
-            printf("\n");
-            break;
+            fclose(arquivo);
+            return 1; // Já existe uma turma com o mesmo código
         }
     }
 
     fclose(arquivo);
+    return 0; // Não há turma com o mesmo código
 }
--- tratadores.h
+++ tratadores.h
@@ -45,10 +45,22 @@ void remover_professor_service();
  turma services
 */
 
-void salvar_turma_service();
-void buscar_turma_service();
-void atualizar_turma_service(Turma *turma);
-void remover_turma_service();
+void cadastrarTurma();
+// Função para exibir as informações de uma turma
+void exibirTurma(const Turma* turma);
+
+// Função para exibir todas as turmas cadastradas
+void exibirTurmas();
+
+// Função para atualizar as informações de uma turma
+void atualizarTurma();
+
+// Função para excluir uma turma
+void excluirTurma();
+
+int verificarCodigoTurma(const char* codigo);
+
+void imprimirTurma(const char* codigo);
 
 /*
     impressoes
