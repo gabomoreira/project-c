16a7cc0500e4044f24928f8dfcab65f13bc0f44b
robertoferreira7 <robertferreira776@gmai.com>
2023-05-30 23:36:54 -0300

    comentarios do dados.h melhorados

--- constantes.h
+++ constantes.h
@@ -1,7 +1,7 @@
 #ifndef _CONSTANTES_H_
 #define _CONSTANTES_H_
 
-#define MAX_ALUNO 100 //estamos definindo uma constante chamada "MAX_ALUNO" e atribuí-la o valor de 100.
+#define MAX_ALUNO 100 //definicao uma constante chamada "MAX_ALUNO" e atribuí-la o valor de 100.
 #define MAX_PROFESSOR 100 // cria uma constante chamada "MAX_PROFESSOR"
 #define MAX_TURMA 100 //cria uma constante chamada "MAX_TURMA"
 
